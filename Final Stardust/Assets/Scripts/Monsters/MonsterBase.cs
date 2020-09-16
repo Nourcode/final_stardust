7eda1dbf39f2fcc5c55d0a78c43291a9c0e04b42
Nour Saidana <noureddinesaidana@gmail.com>
2020-09-16 08:08:38 +0200

    Background + Monster Stats

---
+++ Final Stardust/Assets/Scripts/Monsters/MonsterBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Monster", menuName = "Monster/Create new monster")]
+public class MonsterBase : ScriptableObject
+{
+   [SerializeField] string name;
+
+   [TextArea]
+   [SerializeField] string description;
+
+   [SerializeField] Sprite frontSprite;
+   [SerializeField] Sprite backSprite;
+
+    [SerializeField] MonsterType type1;
+    [SerializeField] MonsterType type2;
+
+    // Base Stats
+    [SerializeField] int maxHP;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int spAttack;
+    [SerializeField] int spDefense;
+    [SerializeField] int speed;
+
+
+
+}
+
+public enum MonsterType
+{
+    None,
+    Venus,
+    Mercury,
+    Mars,
+    Jupiter
+    
+}
--- Final Stardust/Assets/Scripts/Player/PlayerController.cs
+++ Final Stardust/Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+    public LayerMask solidObjectsLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -39,7 +40,9 @@ public class PlayerController : MonoBehaviour
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
-                StartCoroutine(Move(targetPos));
+                if (isWalkable(targetPos)){
+                    StartCoroutine(Move(targetPos));
+                }
             }
         }
 
@@ -58,5 +61,15 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+
+    private bool isWalkable(Vector3 targetPos)
+    {
+        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
