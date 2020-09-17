999309abcfbbb94ce6123c7da21cd13a95b90b81
Nour Saidana <noureddinesaidana@gmail.com>
2020-09-17 07:31:27 +0200

    Creating monster stats + main character monster + legendary

--- Final Stardust/Assets/Scripts/Monsters/MonsterBase.cs
+++ Final Stardust/Assets/Scripts/Monsters/MonsterBase.cs
@@ -6,18 +6,17 @@ using UnityEngine;
 public class MonsterBase : ScriptableObject
 {
    [SerializeField] string name;
-
-   [TextArea]
    [SerializeField] string description;
 
    [SerializeField] Sprite frontSprite;
    [SerializeField] Sprite backSprite;
 
     [SerializeField] MonsterType type1;
-    [SerializeField] MonsterType type2;
 
     // Base Stats
     [SerializeField] int maxHP;
+
+    [SerializeField] int maxMP;
     [SerializeField] int attack;
     [SerializeField] int defense;
     [SerializeField] int spAttack;
@@ -25,6 +24,53 @@ public class MonsterBase : ScriptableObject
     [SerializeField] int speed;
 
 
+    public string Name {
+        get { return name;}
+    }
+
+    public string Description {
+        get { return description;}
+    }
+
+    public Sprite FrontSprite {
+        get { return frontSprite;}
+    }
+
+    public Sprite BackSprite {
+        get { return backSprite;}
+    }
+
+    public MonsterType Type1 {
+        get { return type1;}
+    }
+
+    public int MaxHP {
+        get { return maxHP;}
+    }
+
+    public int MaxMP {
+        get { return maxMP;}
+    }
+
+    public int Attack {
+        get { return attack;}
+    }
+
+    public int Defense {
+        get { return defense;}
+    }
+
+    public int SpAttack {
+        get { return spAttack;}
+    }
+
+    public int SpDefense {
+        get { return spDefense;}
+    }
+
+    public int Speed {
+        get { return speed;}
+    }
 
 }
 
---
+++ Final Stardust/Assets/Scripts/Monsters/Move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move 
+{
+    public MoveBase Base { get; set; }
+    public int MP { get; set;}
+
+    public Move(MoveBase mBase, int mp)
+    {
+        Base = mBase;
+        MP = mp;
+    }
+}
