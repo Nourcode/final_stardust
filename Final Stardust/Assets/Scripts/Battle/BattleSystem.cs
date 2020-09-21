50513b22fe53367dabdf290ffd850549e0e4aaee
Nour Saidana <noureddinesaidana@gmail.com>
2020-09-22 01:12:14 +0200

    DialogBox until playerstate

--- Final Stardust/Assets/Scripts/Battle/BattleSystem.cs
+++ Final Stardust/Assets/Scripts/Battle/BattleSystem.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.UI;
 
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy}
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
@@ -9,16 +11,64 @@ public class BattleSystem : MonoBehaviour
     
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
+
+    //[SerializeField] Image test;
+ 
+    BattleState state;
+    int currentAction;
 
     private void Start() {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    private void SetupBattle()
+    public IEnumerator SetupBattle()
     {
         playerUnit.Setup();
         enemyUnit.Setup();
         playerHud.SetData(playerUnit.Monster);
         enemyHud.SetData(enemyUnit.Monster);
+
+        yield return dialogBox.TypeDialog("Mirashi challenged you to a battle!");
+        yield return new WaitForSeconds(1.5f);
+        yield return dialogBox.TypeDialog("*You draw a card*");
+        yield return new WaitForSeconds(1.5f);
+
+        PlayerAction();
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+        //test.enabled = true;
+    }
+
+    private void Update() {
+        if(state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+    }
+    
+
+    void HandleActionSelection()
+    {
+        Debug.Log(currentAction);
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(currentAction < 1)
+            {
+                ++currentAction;
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(currentAction > 0)
+                --currentAction;
+        }
+        
+        dialogBox.UpdateActionSelection(currentAction);
     }
 }
