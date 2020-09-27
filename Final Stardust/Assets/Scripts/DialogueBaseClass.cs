a455914f69291171537bb7749e520ba0abbd5a4d
Nour Saidana <noureddinesaidana@gmail.com>
2020-09-27 16:47:36 +0200

    audio rival + battle(background)

--- Final Stardust/Assets/Scripts/DialogueBaseClass.cs
+++ Final Stardust/Assets/Scripts/DialogueBaseClass.cs
@@ -10,15 +10,19 @@ public class DialogueBaseClass : MonoBehaviour
     public bool finished { get; private set; }
     protected IEnumerator WriteText(string input, Text textHolder, float delay, AudioClip sound, float delayBetweenLines)
     {
+        SoundManager.instance.PlaySound(sound);
+        
         for(int i = 0; i < input.Length; i++)
         {
             textHolder.text += input[i];
-            //SoundManager.instance.PlaySound(sound);
+            
             yield return new WaitForSeconds(0.03f);
         }
 
         //yield return new WaitForSeconds(delayBetweenLines);
+        
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Z));
+        
         finished = true;
     }
 
